ba945a97a9bac049774d392e09774e13fb55ed87
Alma Wang <almawang96@gmail.com>
2016-08-16 15:36:38 -0400

    cover photo for iframe

--- src/components/bot.js
+++ src/components/bot.js
@@ -5,7 +5,7 @@ const Bot = (props) => {
     <div id="botpage">
       <h1>Meet Dart Bot</h1>
       <div id="bot">
-        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fdartbot&tabs=messages&width=500&height=300&small_header=false&adapt_container_width=true&hide_cover=true&show_facepile=true&appId=1780533645501545"
+        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fdartbot&tabs=messages&width=500&height=300&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=1780533645501545"
           width="500"
           height="300"
           style={{ border: 'none', overflow: 'hidden' }}
