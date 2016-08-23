024e119d98bea0184ced69b24c40403bcd419411
Alma Wang <almawang96@gmail.com>
2016-08-23 17:49:10 -0400

    surge/travis

--- src/components/bot.js
+++ src/components/bot.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Bot = (props) => {
   return (
     <div id="botpage">
-      <h1>Meet Dart Bot</h1>
+      <h1>Meet Dart Bot.</h1>
       <div id="bot">
         <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fdartbot&tabs=messages&width=500&height=600&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=1780533645501545"
           width="500"
